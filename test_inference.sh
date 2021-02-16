5ff18bb23aa0f1b5fcd935b1fa3a0500485b8f53
Jeffrey Chan <jeffrey-chan>
2021-02-15 23:44:05 -0400

    update gitignore

--- test_inference.sh
+++ test_inference.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-process_folder_full_video --videos_path bee/videos --GPU=1 --GPU_mem=12 --tracking hungarian --model_day models/pose/complete_5p_2.best_day.h5 --model_config models/pose/complete_5p_2_model_params.json
+process_folder_full_video --videos_path bee/videos --GPU 1 --GPU_mem 12 --tracking hungarian --model_day new_weights/Inference_model.h5 --model_config new_weights/model_params.json
