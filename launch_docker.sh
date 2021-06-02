5adf2d3d0678bea3e89c6379468eb13da5f9f8e7
Jeffrey Chan <jeffrey-chan>
2021-06-01 20:59:45 -0400

    aoppend username to docker image

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker run --gpus all  -v /home/jchan/beepose:/home/beepose/beepose -v /home/jchan/bee:/home/beepose/beepose/bee -it beepose_image
+docker run --gpus all -v /home/jchan/beepose/:/home/beepose/output_data -v /mnt/storage/work/jchan/tag_dataset_2019:/home/beepose/input_data -v /mnt/storage/work/jchan/beepose_models:/home/beepose/models -it beepose_image_$(whoami)
+# docker run --gpus all -it beepose_image
 
 # docker run --gpus all  -v /home/jchan/beepose_fork:/home/beepose/beepose -v /home/jchan/<input_folder>:/home/beepose/beepose/bee -it beepose_image 
