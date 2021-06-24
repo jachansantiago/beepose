5e0d71d7bceb239937f5fa5e4ca372804cdff8a0
jachansantiago <jeffrey.chan@upr.edu>
2021-06-24 10:59:26 -0400

    add scripts to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='After rewriting the code of the pose estimation I will create a new one. ',
     author='Ivan Felipe rodriguez',
     license='BSD-3',
+    scripts=['scripts/beepose', 'scripts/docker-beepose'],
     entry_points={
         'console_scripts': [
             'train_stages_aug = beepose.train.train_stages_aug:main',
